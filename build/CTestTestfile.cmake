2159b5e06a68e5b6c39f92e8462f3e891cf41d56
dcwalker25 <dcwalker@usc.edu>
2024-02-06 06:45:56 -0800

    Remove arbitrary files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derick/Programming/C++/dom++
-# Build directory: /home/derick/Programming/C++/dom++/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
